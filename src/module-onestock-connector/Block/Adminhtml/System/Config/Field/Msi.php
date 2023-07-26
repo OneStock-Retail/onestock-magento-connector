286397215dad63d2305e18ef35b44c39b299d539
Pascal Noisette <pascal.noisette@smile.fr>
2023-07-26 19:12:11 +0200

    Init module with Adminhtml configuration

---
+++ src/module-onestock-connector/Block/Adminhtml/System/Config/Field/Msi.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Smile\Onestock\Block\Adminhtml\System\Config\Field;
+
+class Msi extends \Magento\Config\Block\System\Config\Form\Field
+{
+    /**
+     * @var \Magento\Framework\Module\Manager
+     */
+    private $moduleManager;
+
+    /**
+     * @var array
+     */
+    private $translate;
+
+    public function __construct(
+        \Magento\Framework\Module\Manager $moduleManager,
+        \Magento\Config\Model\Config\Source\Yesno $yesno,
+        \Magento\Backend\Block\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->moduleManager      = $moduleManager;
+    }
+
+
+    /**
+     * Retrieve the Magento_Inventory status
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * 
+     * @return string
+     */
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        return $this->moduleManager->isEnabled('Magento_Inventory') ? 'Yes' : 'No';
+    }
+}
